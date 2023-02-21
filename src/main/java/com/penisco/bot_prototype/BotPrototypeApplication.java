7878a16fb128553328fab092a14c3624591b4e91
baron <>
2023-02-21 23:26:01 +0300

    initial commit to get things started.

---
+++ src/main/java/com/penisco/bot_prototype/BotPrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.penisco.bot_prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotPrototypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/penisco/bot_prototype/BotPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.penisco.bot_prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
